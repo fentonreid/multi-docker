2de9741c1b2c67c1d4d00eea7a288616a44890ab
Fenton Reid <fentonreid@gmail.com>
2022-05-11 16:24:59 +0100

    Update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/otherpage">Other Page!</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
